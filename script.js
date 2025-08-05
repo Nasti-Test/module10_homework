5ee06903d8feeb1e117cb38a239f0d5cfffc2a6e
Nasti-Test <anastasiachirkova0401@gmail.com>
2025-08-05 22:27:08 +0300

    Добавлены файлы чата

---
+++ script.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const chat = document.getElementById('chat');
+  const messageInput = document.getElementById('massageInput');
+  const sendBtn = document.getElementById('sendBtn');
+  const geoBtn = document.getElementById('geoBtn');
+
+  const socket = new WebSocket('wss://echo-ws-service.herokuapp.com');
+
+  socket.addEventListener('open', (event) => {
+    console.log('Соединение установлено');
+  });
+
+   socket.addEventListener('error', (event) => {
+    console.log('Ошибка соединения:', event);
+  });
+
+  function addMessageToChat(messageInput, isUser = false, isGeo = false) {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+
+    if (isUser) {
+        messageElement.classList.add('user-message');
+    } else if (isGeo) {
+        messageElement.classList.add('geo-message');
+    } else {
+        messageElement.classList.add('server-message');
+    }
+
+    if (isGeo) {
+        const link = document.createElement('a');
+        link.href = message;
+        link.target = '_blank';
+        link.textContent = 'Моя геолокация';
+        messageElement.appendChild(link);
+    } else {
+        messageElement.textContent = message;
+    }
+
+    chat.appendChild(messageElement);
+    chat.scrollTop = chat.scrollHeight;
+  }
+
+  socket.addEventListener('message', (event) => {
+    if (!event.data.startsWith('https://www.openstreetmap.org')) {
+        addMessageToChat(event.data);
+    }
+  })
+
+  sendBtn.addEventListener('click', () => {
+    const message = messageInput.ariaValueMax.trim();
+    if (message) {
+        addMessageToChat(message, true);
+        socket.send(message);
+        messageInput.value = '';
+    }
+  });
+
+  messageInput.addEventListener('keypress', (event) => {
+    if (event.key === 'Event') {
+        sendBtn.click();
+    }
+  });
+
+  geoBtn.addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        addMessageToChat('Геолокация не поддерживается вашим браузером', true);
+        return;
+    }
+
+    geoBtn.disabled = true;
+    geoBtn.textContent = 'Определение...';
+
+    navigator.geolocation.getCurrentPosition(
+        (position) => {
+            const { latitude, longitude } = position.coords;
+            const geoUrl = 'https://www.openstreetmap.org/#map=18/${latitude}/${longitude}';
+            addMessageToChat(geoUrl, true, true);
+
+            socket.send(geoUrl);
+
+            geoBtn.disabled = false;
+            geoBtn.textContent = 'Гео-локация';
+        },
+        (error) => {
+            addMessageToChat('Ошибка получения геолокации: ${error.message}', true);
+            geoBtn.disabled = false;
+            geoBtn.textContent = 'Гео-локация';
+        }
+    );
+  });
+});
